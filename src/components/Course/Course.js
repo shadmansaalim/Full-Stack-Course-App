de4a59bc5279d0e3770762c9185e3588afd685d7
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-04 02:18:58 +0600

    Courses Added

---
+++ src/components/Course/Course.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Card, Col, Button } from 'react-bootstrap';
+import Rating from 'react-rating';
+import './Course.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookOpen } from '@fortawesome/free-solid-svg-icons';
+
+const Course = (props) => {
+    const { name, img, instructor, rating, peopleRated, previousPrice, discountedPrice } = props.course;
+    return (
+        <Col>
+            <Card className="h-100">
+                <Card.Img variant="top" src={img} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        <small>{instructor}</small>
+                        <div className="d-flex justify-content-center mt-1">
+                            <span className="me-1 rating">{rating}</span>
+                            <Rating
+                                className="me-1"
+                                initialRating={rating}
+                                emptySymbol="far fa-star icon-color"
+                                fullSymbol="fas fa-star icon-color"
+                                readonly
+                            ></Rating>
+                            <small>({peopleRated})</small>
+                        </div>
+                        <div className="d-flex justify-content-center mt-2">
+                            <h5 className="fw-bold">${discountedPrice}</h5>
+                            <p className="text-muted ms-1 text-decoration-line-through">${previousPrice}</p>
+                        </div>
+
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <Button variant="primary">Add to Class <FontAwesomeIcon icon={faBookOpen} /></Button>
+                </Card.Footer>
+            </Card></Col>
+    );
+};
+
+export default Course;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,8 @@ import Introduction from '../Introduction/Introduction';
 import { Container } from 'react-bootstrap';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import Course from '../Course/Course';
+import { Row, Col } from 'react-bootstrap';
 
 const Home = () => {
     const [courses, setCourses] = useState([]);
@@ -12,16 +14,20 @@ const Home = () => {
             .then(data => setCourses(data))
 
     }, []);
-    const displayCourses = courses.filter(course => courses.indexOf(course) < 4);
-    console.log(displayCourses);
+    const displayCourses = courses.filter(course => courses.indexOf(course) < 6);
     return (
         <Container>
             <Introduction></Introduction>
-            <section className="my-5">
-                <h2>Services</h2>
-                {
-
-                }
+            <section style={{ marginTop: 120 }}>
+                <h2 className="mb-4">Featured Courses</h2>
+                <Row xs={1} lg={3} className="g-4">
+                    {
+                        displayCourses.map(course => <Course
+                            key={course.name}
+                            course={course}
+                        ></Course>)
+                    }
+                </Row>
             </section>
         </Container>
     );
