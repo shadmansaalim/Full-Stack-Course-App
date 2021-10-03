5e2f0e8abdb6d79f6c546da6107adf45e763a2ae
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-04 00:05:02 +0600

    Component and Router Setup Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Services from './components/Services/Services';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/services">
+            <Services></Services>
+          </Route>
+          <Route exact path="/about">
+            <About></About>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
