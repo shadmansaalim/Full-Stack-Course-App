8244cbf3433eb27616db548376f19de1fbf7aa15
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-04 00:23:29 +0600

    Header Component Completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,29 @@
 import React from 'react';
-
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <Navbar className="shadow-lg" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                        src="https://www.udemy.com/staticx/udemy/images/v7/logo-udemy.svg"
+                        width="80"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <NavLink className="text-decoration-none text-dark me-lg-3" exact to="/home">Home</NavLink>
+                        <NavLink className="text-decoration-none text-dark me-lg-3" exact to="/services">Services</NavLink>
+                        <NavLink className="text-decoration-none text-dark" exact to="/about">About</NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
