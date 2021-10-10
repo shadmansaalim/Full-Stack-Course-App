ff0496feedbce617694760bef012a1e9423899ae
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-11 02:35:48 +0600

    YouTube Component sizing done

--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import useCourses from '../../hooks/useCourses';
 import { useState } from 'react';
 import YouTube from 'react-youtube';
+import { Container } from 'react-bootstrap';
 
 
 const CourseDetails = () => {
@@ -23,62 +24,73 @@ const CourseDetails = () => {
         // selectedCourse && setVideoID((selectedCourse?.video).substring(17));
     }, [courses]);
 
+    const opts = {
+        height: '250',
+        width: '500',
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+    };
+
 
 
     return (
-        <div className="bg-dark text-white p-5">
-            {
-                selectedCourse && <section>
-                    <section className="row mb-5 d-flex mt-5">
-                        <div className="col-lg-6 mx-auto">
-                            <YouTube videoId={selectedCourse.video}>
+        <div className="bg-dark text-white py-5">
+            <Container>
+                {
+                    selectedCourse && <section>
+                        <section className="row mb-5 d-flex mt-5">
+                            <div className="col-lg-6 mx-auto">
+                                <YouTube className="w-100" videoId={selectedCourse.video}>
 
-                            </YouTube>
-                        </div>
-                        <div className="col-lg-6 mx-auto text-start mt-0">
-                            <h3>{selectedCourse.name}</h3>
-                            <p className="mb-2">{selectedCourse.tagline}</p>
-                            <small>
-                                <b>4.5</b>
-                                <Rating
-                                    className="mx-1"
-                                    initialRating={selectedCourse.rating}
-                                    emptySymbol="far fa-star icon-color"
-                                    fullSymbol="fas fa-star icon-color"
-                                    readonly
-                                ></Rating>
-                                ({selectedCourse.peopleRated})
-                            </small>
-                            <br />
-                            <small>Created by <a href="!#">{selectedCourse.instructor}</a></small>
-                            <br />
-                            <button className="btn text-white mt-3" style={{ backgroundColor: 'rgb(69, 82, 110)' }}>Buy Course</button>
-                        </div>
+                                </YouTube>
+                            </div>
+                            <div className="col-lg-5 mx-auto text-start mt-0">
+                                <h3>{selectedCourse.name}</h3>
+                                <p className="mb-2">{selectedCourse.tagline}</p>
+                                <small>
+                                    <b>4.5</b>
+                                    <Rating
+                                        className="mx-1"
+                                        initialRating={selectedCourse.rating}
+                                        emptySymbol="far fa-star icon-color"
+                                        fullSymbol="fas fa-star icon-color"
+                                        readonly
+                                    ></Rating>
+                                    ({selectedCourse.peopleRated})
+                                </small>
+                                <br />
+                                <small>Created by <a href="!#">{selectedCourse.instructor}</a></small>
+                                <br />
+                                <button className="btn text-white mt-3" style={{ backgroundColor: 'rgb(69, 82, 110)' }}>Buy Course</button>
+                            </div>
+
+                        </section>
+                        <section className="mb-4 col-9 mx-auto">
+                            <p className="text-align-justify">
+                                {selectedCourse.description}
+                            </p>
+                        </section>
+                        <section className="mb-4 d-flex align-items-center flex-wrap justify-content-center">
+                            <div className="shadow-lg p-5 rounded-3 me-5 bg-success">
+                                <h5>{selectedCourse.lectures}</h5>
+                                <small>Lectures</small>
+                            </div>
+                            <div className="shadow-lg p-5 rounded-3 me-5 bg-success">
+                                <h5>{selectedCourse.students}</h5>
+                                <small>Students</small>
+                            </div>
+                            <div className="shadow-lg p-5 rounded-3 bg-success">
+                                <h5>{selectedCourse.projects}</h5>
+                                <small>Projects</small>
+                            </div>
+                        </section>
+
 
                     </section>
-                    <section className="mb-4 col-9 mx-auto">
-                        <p className="text-align-justify">
-                            {selectedCourse.description}
-                        </p>
-                    </section>
-                    <section className="mb-4 d-flex align-items-center flex-wrap justify-content-center">
-                        <div className="shadow-lg p-5 rounded-3 me-5 bg-success">
-                            <h5>{selectedCourse.lectures}</h5>
-                            <small>Lectures</small>
-                        </div>
-                        <div className="shadow-lg p-5 rounded-3 me-5 bg-success">
-                            <h5>{selectedCourse.students}</h5>
-                            <small>Students</small>
-                        </div>
-                        <div className="shadow-lg p-5 rounded-3 bg-success">
-                            <h5>{selectedCourse.projects}</h5>
-                            <small>Projects</small>
-                        </div>
-                    </section>
-
-
-                </section>
-            }
+                }
+            </Container>
         </div>
     );
 };
