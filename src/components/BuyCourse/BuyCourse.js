ff84e102639651931b525e8a4ab7fe03e5766968
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-21 16:51:38 +0600

    Code Structured

--- src/components/BuyCourse/BuyCourse.js
+++ src/components/BuyCourse/BuyCourse.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BuyCourse = () => {
     return (
-        <div>
+        <div className="vh-100">
             <h2>This is buy course</h2>
         </div>
     );
