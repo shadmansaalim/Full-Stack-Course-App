7e16bfc5ff1ca30d4bb002d93e9078925130e1ba
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-26 02:59:53 +0600

    Cart data display bug fixed

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { createContext } from 'react';
 import useCart from '../hooks/useCart';
+import useCourses from '../hooks/useCourses';
 import useFirebase from '../hooks/useFirebase';
 
 export const Context = createContext();
 
 const ContextProvider = ({ children }) => {
+    const [courses] = useCourses();
     const authContext = useFirebase();
-    const cartContext = useCart();
+    const cartContext = useCart(courses);
     return (
         <Context.Provider value={[authContext, cartContext]}>
             {children}
