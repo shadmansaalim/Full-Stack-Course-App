cde54e0b3544c8f1c11e0614c308b0818bb49f0a
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-26 15:04:14 +0600

    CountDown Timer Feature built making it full responsive and working 100%

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -2,16 +2,99 @@ import React from 'react';
 import Course from '../Course/Course';
 import { Row, Container } from 'react-bootstrap';
 import useCourses from '../../hooks/useCourses';
+import Countdown from 'react-countdown';
+import './Courses.css'
 
 import { Spinner } from 'react-bootstrap';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Courses = () => {
     const [courses, setCourses, pageCount, activePage, setActivePage] = useCourses();
 
+    const [countdownDay, setCountdownDay] = useState(0);
+    const [countdownHour, setCountdownHour] = useState(0);
+    const [countdownMinute, setCountdownMinute] = useState(0);
+    const [countdownSecond, setCountdownSecond] = useState(0);
+
+    const countdown = () => {
+        const countDate = new Date('Feb 03, 2022 00:00:00').getTime();
+        const now = new Date().getTime();
+        const gap = countDate - now;
+
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+        const textDay = Math.floor(gap / day);
+        setCountdownDay(textDay)
+        const textHour = Math.floor((gap % day) / hour);
+        setCountdownHour(textHour);
+        const textMinute = Math.floor((gap % hour) / minute);
+        setCountdownMinute(textMinute);
+        const textSecond = Math.floor((gap % minute) / second);
+        setCountdownSecond(textSecond);
+    }
+
+    useEffect(() => {
+        setInterval(countdown, 1000);
+    }, [])
 
     return (
         <Container className="mt-5">
-            <h5 className="fw-bold">Check out amazing courses by Top Instructors</h5>
+
+            <h5>Check out Amazing Courses <small className="fw-light">(Up To 60% Off Udemy Discount)</small></h5>
+
+            <section className="mt-2 d-flex justify-content-around col-md-6 col-lg-5 mx-auto">
+                {/* <Countdown date={Date.now() + 10000000000} /> */}
+                <div className="mx-auto">
+                    <div className="d-flex justify-content-center">
+                        <div className="bg-dark text-white p-2 p-lg-3 rounded-3">
+                            <h4 className="m-0"><strong>{parseInt(countdownDay / 10)}</strong></h4>
+                        </div>
+                        <div className="ms-1 bg-dark text-white p-2 p-lg-3  rounded-3">
+                            <h4 className="m-0"><strong>        {parseInt(countdownDay % 10)}</strong></h4>
+                        </div>
+                    </div>
+                    <p>Days</p>
+                </div>
+                <div className="mx-auto">
+                    <div className="d-flex justify-content-center">
+                        <div className="bg-dark text-white p-2 p-lg-3  rounded-3">
+                            <h4 className="m-0"><strong>{parseInt(countdownHour / 10)}</strong></h4>
+                        </div>
+                        <div className="ms-1 bg-dark text-white p-2 p-lg-3  rounded-3">
+                            <h4 className="m-0"><strong>        {parseInt(countdownHour % 10)}</strong></h4>
+                        </div>
+                    </div>
+                    <p>Hours</p>
+                </div>
+                <div className="mx-auto">
+                    <div className="d-flex justify-content-center">
+                        <div className="bg-dark text-white p-2 p-lg-3  rounded-3">
+                            <h4 className="m-0"><strong>{parseInt(countdownMinute / 10)}</strong></h4>
+                        </div>
+                        <div className="ms-1 bg-dark text-white p-2 p-lg-3  rounded-3">
+                            <h4 className="m-0"><strong>        {parseInt(countdownMinute % 10)}</strong></h4>
+                        </div>
+                    </div>
+                    <p>Minutes</p>
+                </div>
+                <div className="mx-auto">
+                    <div className="d-flex justify-content-center">
+                        <div className="bg-dark text-white p-2 p-lg-3  rounded-3">
+                            <h4 className="m-0"><strong>{parseInt(countdownSecond / 10)}</strong></h4>
+                        </div>
+                        <div className="ms-1 bg-dark text-white p-2 p-lg-3  rounded-3">
+                            <h4 className="m-0"><strong>        {parseInt(countdownSecond % 10)}</strong></h4>
+                        </div>
+                    </div>
+                    <p>Seconds</p>
+                </div>
+
+
+            </section>
             {
                 courses.length ?
                     <section>
