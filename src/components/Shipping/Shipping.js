1b170c211c39f7be518d9f177e65b44d128cc18a
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-27 16:26:30 +0600

    Shipping page hook form UI improved with syling

--- src/components/Shipping/Shipping.js
+++ src/components/Shipping/Shipping.js
@@ -5,6 +5,8 @@ import useAuth from '../../hooks/useAuth';
 import useCartContext from '../../hooks/useCartContext';
 import { clearTheCart, getStoredCart } from '../../utilities/LocalStorage';
 import './Shipping.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faForward, faBackward } from '@fortawesome/free-solid-svg-icons';
 
 const Shipping = () => {
     const [cart, setCart] = useCartContext();
@@ -31,29 +33,63 @@ const Shipping = () => {
                     reset();
                 }
             })
-        // history.push('/placeorder');
     };
     const { user } = useAuth();
     const history = useHistory();
 
-    // const handlePlaceOrder = () => {
-    //     history.push('/placeorder');
-    // }
     return (
-        <div className="mt-5 d-flex justify-content-center">
-            <form className="shipping-form" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+            <div className="container mt-5 px-5">
+                <div className="mb-4">
+                    <h2 className="fw-light">Provide Personal Details</h2> <span>Please provide your details in order to confirm subscription of courses.</span>
+                </div>
+                <div className="row">
+                    <div className="col-md-6 mx-auto">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card pay-card p-3">
+                            <h6 className="text-uppercase">User Information</h6>
+                            <div className="inputbox mt-3">
+                                <input defaultValue={user.displayName} {...register("name")} className="form-control pay-control" />
+                                <span>Name</span>
+                            </div>
+                            <div className="inputbox mt-3">
+                                <input type="email" className="form-control pay-control" defaultValue={user.email}  {...register("email", { required: true })} />
+                                <span>Email</span>
+                                {errors.email && <span>This field is required</span>}
+                            </div>
+                            <div className="inputbox mt-3">
+                                <input type="number" className="form-control pay-control" defaultValue="" {...register("phone")} required />
+                                <span>Phone</span>
+                            </div>
 
-                <input placeholder="Your Name" defaultValue={user.displayName} {...register("name")} />
+                            <div className="mt-3 mb-3">
+                                <h6 className="text-uppercase">Billing Address</h6>
+                                <div className="row mt-2">
+                                    <div className="col-md-6">
+                                        <div className="inputbox mt-3 mr-2">
+                                            <input type="text" className="form-control pay-control"
+                                                required="required" defaultValue="" {...register("country")} />
+                                            <span>Country</span> </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="inputbox mt-3 mr-2">
+                                            <input type="text" className="form-control pay-control"
+                                                required="required" defaultValue="" {...register("city")} />
+                                            <span>City</span> </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="mt-4 mb-4 d-flex justify-content-between"> <button onClick={() => history.push('/review')} className="btn btn-secondary px-3"
+                            ><FontAwesomeIcon icon={faBackward} /> Previous</button> <button
+                                className="btn btn-primary px-3">Proceed <FontAwesomeIcon icon={faForward} /></button> </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
 
-                <input placeholder="Your Email" defaultValue={user.email}  {...register("email", { required: true })} />
-                {/* errors will return when field validation fails  */}
-                {errors.email && <span>This field is required</span>}
-                <input placeholder="Address" defaultValue="" {...register("address")} />
-                <input placeholder="City" defaultValue="" {...register("city")} />
-                <input placeholder="Phone" defaultValue="" {...register("phone")} />
 
-                <input type="submit" />
-            </form>
+
+
+
         </div>
     );
 };
