a115b79dad0ecddc8e3e003abb0849c363f6ec44
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-27 23:36:38 +0600

    My classes page done getting user purchased courses from db

--- src/components/MyClasses/MyClasses.js
+++ src/components/MyClasses/MyClasses.js
@@ -4,28 +4,92 @@ import img from '../../images/empty.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 import { useHistory } from 'react-router-dom';
+import './MyClasses.css'
+import { useEffect } from 'react';
+import useAuth from '../../hooks/useAuth'
+import { useState } from 'react';
 
 const MyClasses = () => {
-    // History Api
+
+    const { user } = useAuth();
+    const [myCourses, setMyCourses] = useState([])
+
+    useEffect(() => {
+        if (user.email) {
+            fetch('http://localhost:5000/myClasses', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(user)
+            })
+                .then(res => res.json())
+                .then(result => {
+                    setMyCourses(result);
+                })
+
+        }
+    }, [])
+
+
     const history = useHistory();
     return (
         // MyClasses Page used React FontAwesome for Good UI/UX
-        <section className="text-center mt-5">
+        <section className="text-center mt-5" style={{ marginBottom: '150px' }}>
             <Container>
-                <Row>
-                    <Col lg="6" className="mx-auto shadow-lg mb-5 p-5">
-                        <img className="img-fluid mb-3 col-6" src={img} alt="" />
-                        <h3>No Courses Added</h3>
-                        <p>You have not added any course to your class. Please go back to home or courses and add a course to continue learning.</p>
-                        {/* Navigating the user back to homepage */}
-                        <Button onClick={() => history.push('/home')} variant="outline-primary me-3">Home <FontAwesomeIcon icon={faHome} /></Button>
-                        {/* Navigating the user to courses page so that user can add course */}
-                        <Button onClick={() => history.push('/courses')} variant="outline-primary">Courses <FontAwesomeIcon icon={faAngleDoubleRight} /></Button>
-                    </Col>
-                </Row>
+                {
+                    myCourses.length
+                        ?
+                        <section className="my-5">
+                            <h3 className="fw-bold mb-5">Welcome back <span className="text-primary">{user.displayName}</span>, ready for your next lesson?</h3>
+                            <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4">
+                                {
+                                    myCourses.map(course => {
+                                        console.log(course);
+                                        return (
+                                            <div class="col">
+                                                <div class="card h-100">
+                                                    <img src={course.img} class="card-img-top" alt="..." />
+                                                    <div class="card-body course">
+                                                        <p class="card-title fw-bold">{course.name}</p>
+                                                        <div class="card-text">
+                                                            <small>{course.instructor}</small>
+                                                            <div class="progress col-10 mx-auto mt-2">
+                                                                <div class="progress-bar " role="progressbar" style={{ width: '25%' }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                                                            </div>
+                                                            <button className="btn btn-outline-dark rounded-pill mt-3" >Continue Course</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        );
+                                    })
+                                }
+
+                            </div>
+                        </section>
+                        :
+                        <Row>
+                            <Col lg="6" className="mx-auto shadow-lg mb-5 p-5 rounded-3">
+                                <img className="img-fluid mb-3 col-6" src={img} alt="" />
+                                <h3>No Courses Added</h3>
+                                <p>You have not added any course to your class. Please go back to home or courses and add a course to continue learning.</p>
+                                {/* Navigating the user back to homepage */}
+                                <Button onClick={() => history.push('/home')} variant="outline-primary me-3">Home <FontAwesomeIcon icon={faHome} /></Button>
+                                {/* Navigating the user to courses page so that user can add course */}
+                                <Button onClick={() => history.push('/courses')} variant="outline-primary">Courses <FontAwesomeIcon icon={faAngleDoubleRight} /></Button>
+                            </Col>
+                        </Row>
+                }
             </Container>
         </section>
     );
 };
 
 export default MyClasses;
+
+
+
+
+
+
