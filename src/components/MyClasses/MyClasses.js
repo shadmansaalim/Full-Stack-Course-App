cfa87c090b051e29f5c05d39e627cd1088109c3a
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-28 01:19:13 +0600

    Loading Bug Fixed

--- src/components/MyClasses/MyClasses.js
+++ src/components/MyClasses/MyClasses.js
@@ -8,6 +8,7 @@ import './MyClasses.css'
 import { useEffect } from 'react';
 import useAuth from '../../hooks/useAuth'
 import { useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 
 const MyClasses = () => {
 
@@ -69,17 +70,29 @@ const MyClasses = () => {
                             </div>
                         </section>
                         :
-                        <Row>
-                            <Col lg="6" className="mx-auto shadow-lg mb-5 p-5 rounded-3">
-                                <img className="img-fluid mb-3 col-6" src={img} alt="" />
-                                <h3>No Courses Added</h3>
-                                <p>You have not added any course to your class. Please go back to home or courses and add a course to continue learning.</p>
-                                {/* Navigating the user back to homepage */}
-                                <Button onClick={() => history.push('/home')} variant="outline-primary me-3">Home <FontAwesomeIcon icon={faHome} /></Button>
-                                {/* Navigating the user to courses page so that user can add course */}
-                                <Button onClick={() => history.push('/courses')} variant="outline-primary">Courses <FontAwesomeIcon icon={faAngleDoubleRight} /></Button>
-                            </Col>
-                        </Row>
+                        <>
+                            {
+                                myCourses === 0
+                                    ?
+                                    <Row>
+                                        <Col lg="6" className="mx-auto shadow-lg mb-5 p-5 rounded-3">
+                                            <img className="img-fluid mb-3 col-6" src={img} alt="" />
+                                            <h3>No Courses Added</h3>
+                                            <p>You have not added any course to your class. Please go back to home or courses and add a course to continue learning.</p>
+                                            {/* Navigating the user back to homepage */}
+                                            <Button onClick={() => history.push('/home')} variant="outline-primary me-3">Home <FontAwesomeIcon icon={faHome} /></Button>
+                                            {/* Navigating the user to courses page so that user can add course */}
+                                            <Button onClick={() => history.push('/courses')} variant="outline-primary">Courses <FontAwesomeIcon icon={faAngleDoubleRight} /></Button>
+                                        </Col>
+                                    </Row>
+                                    :
+                                    <div className="vh-100 d-flex flex-column align-items-center mx-auto text-dark" style={{ marginTop: 150 }}>
+                                        <h3 className="text-muted">Loading...</h3>
+                                        <Spinner animation="grow" />
+
+                                    </div>
+                            }
+                        </>
                 }
             </Container>
         </section>
