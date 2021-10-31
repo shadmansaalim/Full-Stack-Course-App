cd534d4600d572b14a0256747d9fd6e9d63c8e15
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-31 17:44:20 +0600

    Updated

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyC2DDVbF4OZtmMR2M02pCLnUavW79YLKyo",
-    authDomain: "react-course-app-de231.firebaseapp.com",
-    projectId: "react-course-app-de231",
-    storageBucket: "react-course-app-de231.appspot.com",
-    messagingSenderId: "982713665692",
-    appId: "1:982713665692:web:1e510cc2fbd93a46e38559"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebaseConfig;
