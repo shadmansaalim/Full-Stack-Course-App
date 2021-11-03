6028a35773b017a237bfcd00841d2f1767090a18
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-11-03 18:55:18 +0600

    Scrolling Bug Fixe

--- src/components/ScrollToTop/ScrollToTop.js
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = ({ children }) => {
     const { pathname } = useLocation();
-
+    console.log(pathname);
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [pathname]);
