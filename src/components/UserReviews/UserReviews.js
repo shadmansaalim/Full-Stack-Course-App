ff25bc1f49ea14263f348e53a060bec634c7e422
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-11-25 00:59:44 +0600

    Double showling review bug fixed

--- src/components/UserReviews/UserReviews.js
+++ src/components/UserReviews/UserReviews.js
@@ -9,42 +9,6 @@ import './UserReviews.css';
 
 
 
-const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    initialSlide: 0,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    swipeToSlide: true,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: true,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
-};
 
 
 const UserReviews = ({ id }) => {
@@ -54,6 +18,46 @@ const UserReviews = ({ id }) => {
             .then(res => res.json())
             .then(data => setReviews(data[0]?.reviews));
     }, [id])
+
+    const slideCount = reviews.length < 2 ? 1 : 2
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: slideCount,
+        slidesToScroll: slideCount,
+        initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        swipeToSlide: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: slideCount,
+                    slidesToScroll: slideCount,
+                    infinite: true,
+                    dots: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
     return (
         <>
             {
@@ -62,7 +66,7 @@ const UserReviews = ({ id }) => {
                 <div>
                     <h1 className="fw-bold mb-5" style={{ fontSize: '42px' }}>Course Reviews</h1>
 
-                    <Slider {...settings} className="col-lg-10 mx-auto mt-lg-4 mb-5">
+                    <Slider {...settings} className={slideCount === 1 ? "col-lg-5 mx-auto mt-lg-4 mb-5" : "col-lg-10 mx-auto mt-lg-4 mb-5"}>
 
                         {
                             reviews.map(review => <UserReview
